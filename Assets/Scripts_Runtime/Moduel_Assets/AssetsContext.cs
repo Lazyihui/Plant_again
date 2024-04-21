6cb984925a87833b1ac98d8ed08df8c78d36dcdc
包子 <chengbaozi6@qq.com>
2024-04-21 23:27:07 +0800

    <feature>add:assetscontext

--- Assets/Scripts_Runtime/Moduel_Assets/AssetsContext.cs
+++ Assets/Scripts_Runtime/Moduel_Assets/AssetsContext.cs
@@ -1,18 +1,29 @@
 using UnityEngine;
 using System.Collections.Generic;
+using UnityEngine.Tilemaps;
+using UnityEngine.ResourceManagement.AsyncOperations;
 
 public class AssetsContext {
-    public Dictionary<string, GameObject> entities;
+    public Dictionary<string, GameObject> entity;
 
-    public Dictionary<string, GameObject> panels;
+    public AsyncOperationHandle entitiyHandle;
+
+    public Dictionary<string, GameObject> ui;
+
+    public AsyncOperationHandle uiHandle;
+
 
     public AssetsContext() {
-        entities = new Dictionary<string, GameObject>();
-        panels = new Dictionary<string, GameObject>();
+        entity = new Dictionary<string, GameObject>();
+        ui = new Dictionary<string, GameObject>();
     }
 
     public bool Panel_TryGetPrefab(string name, out GameObject prefab) {
-        return panels.TryGetValue(name, out prefab);
+        return ui.TryGetValue(name, out prefab);
+    }
+
+    public bool Entity_TryGetPrefab(string name, out GameObject prefab) {
+        return entity.TryGetValue(name, out prefab);
     }
 
 }
--- Assets/Scripts_Runtime/Moduel_Assets/AssetsCore.cs
+++ Assets/Scripts_Runtime/Moduel_Assets/AssetsCore.cs
@@ -1,1 +1,46 @@
 using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+
+public static class AssetsCore {
+
+    public static void Load(AssetsContext ctx) {
+
+        {
+
+            AssetLabelReference labelReference = new AssetLabelReference();
+            labelReference.labelString = "Entity";
+            var handle = Addressables.LoadAssetsAsync<GameObject>(labelReference, null);
+            var list = handle.WaitForCompletion();
+            foreach (var go in list) {
+                ctx.entity.Add(go.name, go);
+            }
+            ctx.entitiyHandle = handle;
+        }
+        {
+
+            AssetLabelReference labelReference = new AssetLabelReference();
+            labelReference.labelString = "UI";
+            var handle = Addressables.LoadAssetsAsync<GameObject>(labelReference, null);
+            var list = handle.WaitForCompletion();
+            foreach (var go in list) {
+                ctx.ui.Add(go.name, go);
+            }
+            ctx.uiHandle = handle;
+        }
+    }
+
+    public static void ReleaseAll(AssetsContext ctx) {
+        if (ctx.entitiyHandle.IsValid()) {
+            Addressables.Release(ctx.entitiyHandle);
+        }
+        if (ctx.uiHandle.IsValid()) {
+            Addressables.Release(ctx.uiHandle);
+        }
+    }
+
+}
